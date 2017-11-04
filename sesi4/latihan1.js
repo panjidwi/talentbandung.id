93f24d30746491c45aed178758ea5e2914aaee05
panji <panjidwi1437@gmail>
2017-11-04 18:29:20 +0700

    Penambahan sesi 4

---
+++ sesi4/latihan1.js
@@ -0,0 +1,25 @@
+alert("hellow");
+var nama = "Panji Dwi";
+var alamat ="Bandung";
+
+var webBasic = ["Apel", "Ceri", "Durian", "Anggur", "Sirsak", "Rambutan", "Nangka"];
+
+console.log(webBasic[0]);
+console.log(webBasic[4]);
+webBasic[4] = "Anggur Merah";
+console.log(webBasic[4]);
+
+var person = {
+    nama: 'Panji',
+    age: 22,
+    address: 'Antapani',
+    gadget: {
+        laptop: 'Axioo',
+        phone: 'S4'
+    },
+    hobby: ['Nonton', 'Baca']
+};
+
+console.log( Person.name )
+console.log( person.gadgets.laptop )
+console.log( person.hobby[1] )
