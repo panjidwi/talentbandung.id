da8473066de4a4ccaec1feaebb6b518228b25d2d
panji <panjidwi1437@gmail>
2017-11-26 20:03:49 +0700

    Update

--- kulinerbandung/assets/js/apps.js
+++ kulinerbandung/assets/js/apps.js
@@ -1,4 +1,18 @@
-//loading screen
+/* loading screen
 	$(window).load(function() {
 		$(".se-pre-con").fadeOut("slow");;
-	});
+    }); */
+    
+//scroll animation
+AOS.init();
+
+/*list makanan
+var listMakanan = $('.content-item');
+$.get ('https://panjidwi.github.io/talentbandung.id/kulinerbandung/assets/js/kuliner.json', function (data, status) {
+    for (var i=0; data.length; i++ ) {
+        var makanan = '<div class="item">'+'<article>'+'<a href="article.html">'+'<h2>'+data[i].nama+'</h2>'+'<p>'+data[i].deskripsi+'</a>'+'</div>';
+        listMakanan.append(makanan);
+    }
+}) */
+
+/*filter*/
