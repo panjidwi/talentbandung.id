016343f78ec0d93a6cad0cde6bd50024f4e27b90
panji <panjidwi1437@gmail>
2017-11-27 15:32:46 +0700

    Update

--- kulinerbandung/assets/js/apps.js
+++ kulinerbandung/assets/js/apps.js
@@ -1,18 +1,11 @@
-/* loading screen
-	$(window).load(function() {
-		$(".se-pre-con").fadeOut("slow");;
-    }); */
-    
 //scroll animation
 AOS.init();
 
 /*list makanan
 var listMakanan = $('.content-item');
-$.get ('https://panjidwi.github.io/talentbandung.id/kulinerbandung/assets/js/kuliner.json', function (data, status) {
+$.get ('https://panjidwi.github.io/talentbandung.id/kulinerbandung/assets/json/kuliner.json', function (data, status) {
     for (var i=0; data.length; i++ ) {
-        var makanan = '<div class="item">'+'<article>'+'<a href="article.html">'+'<h2>'+data[i].nama+'</h2>'+'<p>'+data[i].deskripsi+'</a>'+'</div>';
+        var makanan = '<div class="item">'+'<figure>'+'<img src="'+data[i].images+'" />'+'</figure>'+'<article>'+'<a href="'+data[i].link+'">'+'<h2>'+data[i].nama+'</h2>'+'<p>'+'<strong>'+data[i].kategori+'</strong>'+'</p>'+'<p>'+data[i].deskripsi+'</p>'+'</a>'+'<article>'+'</div>'
         listMakanan.append(makanan);
     }
-}) */
-
-/*filter*/
+})*/
