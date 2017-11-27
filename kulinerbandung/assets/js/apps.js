8a7c9b7cf202f0f2ce377c536700321d39e11637
panji <panjidwi1437@gmail>
2017-11-27 20:13:42 +0700

    Commit

--- kulinerbandung/assets/js/apps.js
+++ kulinerbandung/assets/js/apps.js
@@ -8,4 +8,4 @@ $.get ('https://panjidwi.github.io/talentbandung.id/kulinerbandung/assets/json/k
         var makanan = '<div class="item">'+'<figure>'+'<img src="'+data[i].images+'" />'+'</figure>'+'<article>'+'<a href="'+data[i].link+'">'+'<h2>'+data[i].nama+'</h2>'+'<p>'+'<strong>'+data[i].kategori+'</strong>'+'</p>'+'<p>'+data[i].deskripsi+'</p>'+'</a>'+'</article>'+'</div>';
         listMakanan.append(makanan);
     }
-})
+});
