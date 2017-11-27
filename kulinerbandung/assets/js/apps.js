45d248afd1e9bfc3dc99892dd1c3256a3f00b7e1
panji <panjidwi1437@gmail>
2017-11-27 18:46:10 +0700

    Commit apps.js

--- kulinerbandung/assets/js/apps.js
+++ kulinerbandung/assets/js/apps.js
@@ -1,11 +1,11 @@
 //scroll animation
 AOS.init();
 
-/*list makanan
+//list makanan
 var listMakanan = $('.content-item');
 $.get ('https://panjidwi.github.io/talentbandung.id/kulinerbandung/assets/json/kuliner.json', function (data, status) {
     for (var i=0; data.length; i++ ) {
-        var makanan = '<div class="item">'+'<figure>'+'<img src="'+data[i].images+'" />'+'</figure>'+'<article>'+'<a href="'+data[i].link+'">'+'<h2>'+data[i].nama+'</h2>'+'<p>'+'<strong>'+data[i].kategori+'</strong>'+'</p>'+'<p>'+data[i].deskripsi+'</p>'+'</a>'+'<article>'+'</div>'
+        var makanan = '<div class="item">'+'<figure>'+'<img src="'+data[i].images+'" />'+'</figure>'+'<article>'+'<a href="'+data[i].link+'">'+'<h2>'+data[i].nama+'</h2>'+'<p>'+'<strong>'+data[i].kategori+'</strong>'+'</p>'+'<p>'+data[i].deskripsi+'</p>'+'</a>'+'</article>'+'</div>'
         listMakanan.append(makanan);
     }
-})*/
+})
